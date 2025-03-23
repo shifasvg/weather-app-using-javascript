4962311026adccd36218c211e02d5c1bf8329494
shifasvg <shifasvg1@gmail.com>
2025-03-23 23:26:49 +0530

    completed

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city  + `&appid=${apiKey}`);
-    var data = await response.json();
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }else{
+        var data = await response.json();
     
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°c";
@@ -29,6 +34,10 @@ async function checkWeather(city) {
     }
 
     document.querySelector('.weather').style.display="block"
+    document.querySelector(".error").style.display="none";
+    }
+
+    
 }
 
 searchBtn.addEventListener("click",()=>{
